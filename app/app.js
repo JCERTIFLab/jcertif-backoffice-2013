0df58e1bebde2e26b1c53d15b8f8d0a121c95af5
Chrisbel <rmalonga9@gmail.com>
2013-03-14 01:06:04 +0100

    Ajustement de la page de Bienvenu de l'application

--- app/app.js
+++ app/app.js
@@ -3,16 +3,6 @@ Ext.application({
     controllers: ["Main"],
     views: ["Main"],
     name: 'JCertifAdmin',
-    launch: function() {
-        Ext.create('Ext.container.Viewport', {
-            layout: 'fit',
-            items: [
-                {
-                    xtype: 'panel',
-                    title: 'Interface administration de JCertif',
-                    html : 'Ensemble Innovons, bienvenu dans le projet JCertif Back-Office'
-                }
-            ]
-        });
-    }
+    autoCreateViewport: true
+   
 });
--- app/view/Viewport.js
+++ app/view/Viewport.js
@@ -1,12 +1,26 @@
 Ext.define('JCertifAdmin.view.Viewport', {
-			renderTo : Ext.getBody(),
-			extend : 'Ext.container.Viewport',
-			requires : ['Ext.tab.Panel', 'Ext.layout.container.Border'],
+	renderTo : Ext.getBody(),
+	extend : 'Ext.container.Viewport',
+	requires : ['Ext.tab.Panel', 'Ext.layout.container.Border'],
+	initComponent : function() {
+
+		var me = this;
+		Ext.applyIf(me, {
 
 			items : [{
-						xtype : 'panel',
-						layout : 'fit',
-						title : 'Interface administration de JCertif '
-						
-					}]
+				xtype : 'panel',
+				layout : 'fit',
+				bodyPadding: 10,
+				collapsible :true,
+				title : 'Interface administration de JCertif ',
+				html : '<h1>Bienvenu sur le projet communautaire OPEN-SOURCE avec EXTJS 4</h1>',
+				height :500,
+				width : 600
+			}]
+
 		});
+
+		me.callParent(arguments);
+	}
+
+});
