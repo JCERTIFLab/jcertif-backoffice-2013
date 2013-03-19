e978daaca43fc64781192efd59c0735e77b6af8f
Chrisbel <rmalonga9@gmail.com>
2013-03-19 01:40:53 +0100

    Mise en place du premier test d'écran du Login administrateur

--- app/view/Viewport.js
+++ app/view/Viewport.js
@@ -1,23 +1,63 @@
+var fsf = Ext.widget({
+	xtype : 'form',
+	id : 'fieldSetForm',
+	collapsible : true,
+	url : 'save-form.php',
+	frame : true,
+	title : 'Connectez-vous ici',
+	bodyPadding : '30 30 30 30',
+	width : 150,
+	fieldDefaults : {
+		msgTarget : 'side',
+		labelWidth : 50
+	},
+	defaults : {
+		anchor : '50%'
+	},
+
+	items : [ {
+		xtype : 'fieldset',
+		title : 'Identification',
+		collapsible : true,
+		defaultType : 'textfield',
+		layout : 'anchor',
+		items : [ {
+			fieldLabel : 'Login',
+			name : 'home',
+			value : 'login'
+		} ]
+
+	}],
+	buttons: [{
+        text: 'Save'
+    },{
+        text: 'Cancel'
+    }]
+
+});
+
 Ext.define('JCertifAdmin.view.Viewport', {
 	renderTo : Ext.getBody(),
 	extend : 'Ext.container.Viewport',
-	requires : ['Ext.tab.Panel', 'Ext.layout.container.Border'],
+	requires : [ 'Ext.tab.Panel', 'Ext.layout.container.Border' ],
+
 	initComponent : function() {
 
 		var me = this;
 		Ext.applyIf(me, {
-
-			items : [{
+			xtype : 'panel',
+			width : 300,
+			height : 200,
+			title : 'Border Layout',
+			layout : 'border',
+			items : [ {
+				title : 'Interface de gestion administration JCertif',
+				region : 'center', // center region is required,
 				xtype : 'panel',
 				layout : 'fit',
-				bodyPadding: 10,
-				collapsible :true,
-				title : 'Interface administration de JCertif ',
-				html : '<h1>Bienvenu sur le projet communautaire OPEN-SOURCE avec EXTJS 4</h1>',
-				height :500,
-				width : 600
-			}]
-
+				margins : '30 30 30 30',
+				items : [ fsf ]
+			} ]
 		});
 
 		me.callParent(arguments);
