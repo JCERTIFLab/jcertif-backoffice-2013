d0304a7a0d36fb606f9d6b1853496aacbebff036
Martial SOMDA <martialsomda@gmail.com>
2013-08-15 21:41:07 +0200

    Modification de la conf de social login

--- app/store/AuthProviders.js
+++ app/store/AuthProviders.js
@@ -6,6 +6,6 @@ Ext.define('JCertifBO.store.AuthProviders', {
 	data: [
         {name: 'userpass', authorizationUrl: BACKEND_URL + '/admin', accessTokenUrl: BACKEND_URL + '/token/new', requestTokenUrl: BACKEND_URL + '/token/new'},
         {name: 'google', authorizationUrl: 'https://accounts.google.com/o/oauth2/auth', accessTokenUrl: 'https://accounts.google.com/o/oauth2/token', clientId: '854354618002.apps.googleusercontent.com', scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'},
-        {name: 'github', authorizationUrl: 'https://github.com/login/oauth/authorize', accessTokenUrl: 'https://github.com/login/oauth/access_token', clientId: '2d0dfe17261d3b10583c', clientSecret: 'ba91b90968bb0221b1a10d142e27bb8773a9a40d', scope: 'user:email'}
+        {name: 'github', authorizationUrl: 'https://github.com/login/oauth/authorize', accessTokenUrl: 'https://github.com/login/oauth/access_token', clientId: '672000c39117d988c3d9', clientSecret: 'e97fdd89ec9c08d27c4de56444f6908c285fcda3', scope: 'user:email'}
     ]
 });
