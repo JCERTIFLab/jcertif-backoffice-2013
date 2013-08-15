a2534800787f543c277c01eb84ab3e27dfcecb29
Martial SOMDA <martialsomda@gmail.com>
2013-08-15 21:37:02 +0200

    Incrementation de la version de l'application

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 BACKEND_URL='http://jcertif-backend.msomda.cloudbees.net';
 BACKOFFICE_URL='http://jcertiflab.github.io/jcertif-backoffice-2013';
-VERSION='0.2-SNAPSHOT';
+VERSION='0.3-SNAPSHOT';
 
 var splashscreen;
 
