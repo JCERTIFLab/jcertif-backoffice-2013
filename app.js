92f98a05570f02a25cbd185edce586ddc78a708d
Rossi Oddet <rossi.oddet@gmail.com>
2013-09-04 18:04:15 -0400

    Passage a la prochaine iteration

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var BACKEND_URL='http://jcertif-backend.msomda.cloudbees.net';
 var BACKOFFICE_URL='http://jcertiflab.github.io/jcertif-backoffice-2013';
-var VERSION='0.6';
+var VERSION='0.7-SNAPSHOT';
 
 var splashscreen;
 
